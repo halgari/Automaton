5b102950fa1e61c66d132df673e6a182145906c2
Ethan Green <greenmarleyethan@gmail.com>
2018-03-17 23:49:37 -0400

    Documentation on handler

--- src/Automaton/Model/Handles/PackHandler.cs
+++ src/Automaton/Model/Handles/PackHandler.cs
@@ -8,6 +8,12 @@ namespace Automaton
 {
     class PackHandler
     {
+
+        /// <summary>
+        /// Returns a list of type <see cref="Mod"/> which contains mods that were not able to be found in the standard source directory
+        /// </summary>
+        /// <param name="sourceDirectory"></param>
+        /// <returns></returns>
         public static List<Mod> GetMissingMods(string sourceDirectory)
         {
 
